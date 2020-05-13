9ec55ea5c8a1c56bbeb333c6e6014314d9cf5d49
PeterYCZ <yecongzhoupeter@qq.com>
2020-05-13 13:05:06 +0800

    update ListNode.java

--- src/com/learning/Main.java
+++ src/com/learning/Main.java
@@ -4,9 +4,32 @@ import com.learning.TwoSum.SolutionHash;
 
 public class Main {
 
+    public static void add (test x){
+        x.setX(x.getX()+1);
+    }
+
+    static class test{
+
+        private int x;
+
+        test(int x){
+            this.x = x;
+        }
+
+        public void setX(int x){
+            this.x = x;
+        }
+
+        public int getX(){
+            return x;
+        }
+
+    }
+
     public static void main(String[] args) {
-        char x = '1';
-        System.out.println(Integer.valueOf(x));
-        System.out.println("Hello World!");
+        test y = new test(1);
+        add(y);
+        System.out.println(y.getX());
+
     }
 }
